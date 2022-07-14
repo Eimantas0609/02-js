5a9a7d3c384296f5a38e010645950a8890de8a16
Eimantas0609 <100865215+Eimantas0609@users.noreply.github.com>
2022-07-14 16:25:38 +0300

    callback & destructuring

---
+++ js/destructuring.js
@@ -0,0 +1,13 @@
+// const [first, second, ...other] = daiktai;
+
+const person = {
+	name: "Petras",
+	age: 99,
+	isMarried: true,
+};
+
+const { age, name, isMarried } = person;
+
+console.log(name);
+console.log(age);
+console.log(personOtherInfo);
